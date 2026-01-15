d3c1a3548af33a71d883b8352f1a162e453b99d3
patrickgiry <patrickgiry@gmail.com>
2026-01-15 19:26:05 +0100

    Transform `print` method extension to a property extension

--- src/main/kotlin/ClientDsl.kt
+++ src/main/kotlin/ClientDsl.kt
@@ -2,6 +2,5 @@ package kata
 
 import kata.kotlin.dsl.Client
 
-fun Client.print(): String {
-    return this.bluesky().handle() + " " + this.company().name()
-}
+val Client.print: String
+    get() =  this.bluesky().handle() + " " + this.company().name()
