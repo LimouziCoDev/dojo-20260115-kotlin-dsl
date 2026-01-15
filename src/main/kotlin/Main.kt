07d08bb5e417a7a5fd3fdc04e0c379d185fa6ae4
patrickgiry <patrickgiry@gmail.com>
2026-01-15 19:09:19 +0100

    Convert to kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,26 @@
 package kata
 
+import kata.kotlin.dsl.Bluesky
+import kata.kotlin.dsl.Client
+import kata.kotlin.dsl.Company
+import java.time.LocalDate
+import java.time.Month
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val client = Client(
+        "Jean",
+        "DUPOND",
+        Company(
+            "Company",
+            "Town"
+        ),
+        Bluesky(
+            "@jeandupond.bsky.social"
+        ),
+        LocalDate.of(2002, Month.MARCH, 20)
+    )
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+    println("Created client is : $client")
 }
